a2300e4bb59039fb353e1cf4bf2c729efae36508
SirFuryy <giacomotommi@gmail.com>
2024-10-09 15:34:12 +0200

    nuovi appunti

--- esercizzi altri/esercizi-giacomo-guidi/prova.ml
+++ esercizzi altri/esercizi-giacomo-guidi/prova.ml
@@ -7,9 +7,11 @@ a = inc 2;;
 let foo = 1 ;;
 let foo' = foo * 2 ;;
 
-(*In ocaml non è necessario definire i tipi perchè li riconosce luiù
+(*In ocaml non è necessario definire i tipi perchè li riconosce lui
+   puoi comunque definirli attraverso (x: int) o (x: float) ecc...
 *)
-let inc_int (x: int) : int = x + 1 ;;
+let inc_int (x: float) = x +. 1. ;;
+let inc_float (x: int) : int = x + 1 ;; (* così gli definisco anche il tipo del valore di ritorno*)
 let inc_int' x = x+1;;
 let inc_int'' = function x -> x+1;;
 
@@ -31,6 +33,46 @@ let print_hello () = print_endline "hello world" ;;
 (* Devi ovviamente scrivere () nelle chiamate a funzioni zerarie *)
 print_hello () ;;
 
+(* Puoi usare più espressioni all'interno del copro. L'ultima di queste diventa il return value. 
+   Tutte le altre espressioni devono essere di tipo "unit" *)
+   let print_and_return x =
+      print_endline (string_of_int x);
+      x
+  ;;
+
 (* Le istanazazioni possono essere unite attraverso il construtto "let ... in".
    E come assegnare valori multipli a più variabili nei linguaggi imperativi. *)
-let x = 10 in let y = 20 in x + y ;;
+   let x = 10 in let y = 20 in x + y ;;
+
+(* Puoi usare anche il construtto "let ... and ... in". È utile per le funzioni ricorsive che
+   si chiamano a vicenda. Se provi a farlo con il costrutto "let ... in", il compilatore si lamenterà di valori non definiti. *)
+let rec
+   is_even = function
+   | 0 -> true
+   | n -> is_odd (n-1)
+   and
+   is_odd = function
+   | 0 -> false
+   | n -> is_even (n-1);;
+
+(* Puoi definire nuovi operatori o ridefinirne di esistenti chiamando let e l'operatore 
+   tra parentesi. Solo alcuni simboli possono essere usati.
+   Puoi determinare l'associatività e le regole di precedenza dell'operazione mediante il primo 
+   simbolo dell'operatore *)
+let (+) a b = a - b ;; 
+
+(* Esempio: operatori unari devono inizia con il simbolo "~" *)
+let (~/) x = 1.0 /. x ;;
+~/4.0 (* = 0.25 *);;
+
+(* Esempio: operatori binari devono iniziare con uno dei simboli: + - * / < > @ ^ | & *)
+let (++) a b = a ^ b ;;
+
+(*Appunti veloci su liste
+   Puoi accedere ad elementi individuali delle liste con la funzione List.nth *)
+let my_list = [1; 2; 3] ;;
+List.nth my_list 1 ;;
+
+(* Ci sono funzioni di alto livello nella libreria List che semplificano il lavoro: map and filter. *)
+List.map (fun x -> x * 2) [1; 2; 3] ;;
+List.filter (fun x -> x mod 2 = 0) [1; 2; 3; 4] ;;
